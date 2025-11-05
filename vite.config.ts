82b2005158721ebd33d09e480432e2ffdefacacd
Nguyễn Chí Lương <ncluong21@clc.fitus.edu.vn>
2025-11-05 20:56:15 +0700

    added base to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/HRMS-FE",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
