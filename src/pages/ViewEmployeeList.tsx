e716301dfac8c4de2a03740db50d5c15150e0838
Nguyễn Chí Lương <ncluong21@clc.fitus.edu.vn>
2025-11-25 21:43:46 +0700

    build-able?

--- src/pages/ViewEmployeeList.tsx
+++ src/pages/ViewEmployeeList.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useEmployeeStore, type Employee } from '@/store/useStore';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+// import { useNavigate } from 'react-router-dom';
+// import { useEmployeeStore, type Employee } from '@/store/useStore';
+// import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+// import { Input } from '@/components/ui/input';
 import { Users, Funnel} from "lucide-react"
 
 function TitleBar() {
