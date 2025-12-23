12edd61a742d87726f7705f1189817a96a90e900
sinful-johnny <125457000+sinful-johnny@users.noreply.github.com>
2025-12-23 08:15:20 +0700

    HRMS-81: employee personal info page (#12)

--- src/pages/employeeProfileManagement/pages/PersonalInfo.tsx
+++ src/pages/employeeProfileManagement/pages/PersonalInfo.tsx
@@ -1,6 +1,8 @@
 import NavigationBar from "@/pages/employeeProfileManagement/components/NavigationBar";
 import EmployeeSidebar from "@/pages/employeeProfileManagement/components/EmployeeSidebar";
 import ProfileTabs from "@/pages/employeeProfileManagement/components/ProfileTabs";
+import PersonalDetails from "@/pages/employeeProfileManagement/components/PersonalDetails";
+import "./EmployeeIDs.standalone.css";
 
 export default function PersonalInfo() {
   return (
@@ -13,10 +15,7 @@ export default function PersonalInfo() {
         <main className="flex-1 bg-hrms-bg-page min-h-[calc(100vh-85px)] p-4 lg:p-[30px]">
           <div className="flex flex-col gap-[30px] max-w-[1177px]">
             <ProfileTabs />
-            <div className="bg-white rounded-[20px] p-[30px]">
-              <h2 className="text-2xl font-semibold mb-4">Personal Information</h2>
-              <p className="text-gray-600">Personal information content coming soon...</p>
-            </div>
+            <PersonalDetails />
           </div>
         </main>
       </div>
