a41e7dac344bf90eac52d80d1f82d3386b1b2650
Kiet Nguyen <98326962+kjetng@users.noreply.github.com>
2026-01-09 18:21:28 +0700

    feat: finish the feature (#61)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { RouterProvider } from 'react-router-dom';
 import LoadingScreen from './components/LoadingScreen';
 import { Router } from './routes';
 import { Toaster } from "sonner";
+import ToastProvider from './components/ToastProvider';
 
 function App() {
   return (
     <Suspense fallback={<LoadingScreen />}>
       <Toaster position="top-right" richColors />
+      <ToastProvider />
       <RouterProvider router={Router} />
     </Suspense>
   );
